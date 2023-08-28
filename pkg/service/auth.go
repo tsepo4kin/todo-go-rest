a869c405cbc11a9642a59acfbac2bab948eb4302
Kirill Tseplyaev <tseplyaev@lanit.ru>
2023-08-28 19:16:30 +0300

    feat(auth): user registration

---
+++ pkg/service/auth.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+
+	"github.com/tsepo4kin/todo-go-rest/pkg/repository"
+	"github.com/tsepo4kin/todo-go-rest/types"
+)
+
+const salt = ";lkajsdghui123ijvasduiv98"
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
