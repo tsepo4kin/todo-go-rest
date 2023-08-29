f120265b3e8ef970d1a105a26bc7882139a22ece
Kirill Tseplyaev <tseplyaev@lanit.ru>
2023-08-29 12:05:45 +0300

    feat(docs) add swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,11 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tsepo4kin/todo-go-rest/pkg/service"
+
+	"github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+
+	_ "github.com/tsepo4kin/todo-go-rest/docs"
 )
 
 type Handler struct {
@@ -15,6 +20,8 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	auth := router.Group("/auth")
 	{
